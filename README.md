a26374620659d2a103ab3b6a16e6cef98c2fd21c
KanishkKalyani <kanishkkalyani@gmail.com>
2021-01-04 21:00:11 +0530

    Added Cloudinary APIs and automated sprite generation

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 ## Instructions for use:
 
 - Run 'npm i' in command line after cloning the repository locally and navigating to it.
-- Add the images in './src/icons' folder to be included in the spritesheet.
-- Create a folder named 'output' in 'src' folder.
-- Change the fileName in line 15 in index.js file to appropriate file name in context with your use.
-- Change the 'algorithm' in line 26 in index.js file to one of the algorithms listed above that line.
-- Change the 'padding' in line 31 in index.js file.
-- Run 'npm start' in command line.
-- The './src/output' folder will contain the new generated sprite image and a CSS file containing the classNames which can be used directly to use the icons from the Sprite Sheet.
+- Add the .env file
+- Run 'npm start' and enter the asked details.
+- The './src/output' folder will contain the new generated sprite image and a CSS file containing the classNames which can be used directly to use the images from the Sprite Sheet.
+- The Sprite Image will also be uploaded to the cloudinary Assets part.
 
 ### Refer https://www.npmjs.com/package/spritesmith for further details
---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const { default: axios } = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const prompt = require('prompt');
+
+dotenv.config(); 
+
+// Middleware
+app.use(express.json());
+
+// Route
+app.use('/user', require('./routes/user'));
+
+let folderName, outputFileName;
+
+const startDownload = () => {
+  return new Promise(function (resolve, reject) {
+    axios.get('http://localhost:5000/user', {
+    params: {
+      folderName
+    }
+  })
+  .then((response) => {
+    console.log(response);
+    resolve(response)}
+  )
+.catch((error) => reject(error));
+  });
+}
+
+const startSpriteGeneration = () => {
+  return new Promise(function (resolve, reject) {
+    axios.post('http://localhost:5000/user/generate-sprite', {
+        fileName: outputFileName,
+        algorithm: "binary-tree",
+        padding: 1
+    })
+    .then((response) => {
+      console.log(response);
+      resolve(response)}
+    )
+    .catch((error) => reject(error));
+  });
+}
+
+const startUploadingSprite = () => {
+  let form = new FormData();
+  form.append('image', fs.createReadStream(`./src/output/${outputFileName}.png`));
+  form.append('outputFileName', outputFileName);
+
+  return new Promise(function (resolve, reject) {
+    axios.post('http://localhost:5000/user', form, {
+      headers: {
+        'accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Content-Type': `multipart/form-data; boundary=${form._boundary}`,
+      }
+    })
+    .then((response) => {
+      console.log(response);
+      resolve(response)}
+    )
+    .catch((error) => reject(error));
+  });
+}
+
+const clearDirectories = () => {
+  fs.rmdir("./src/downloads", { 
+    recursive: true, 
+  }, (error) => { 
+    if (error) { 
+      console.log(error); 
+    } 
+    else { 
+      console.log("Non Recursive: Directories Deleted!"); 
+    } 
+  });
+}
+
+function start() {
+  startDownload();
+  setTimeout( () => {
+    startSpriteGeneration();
+  }, 3000);
+  setTimeout( () => {
+    startUploadingSprite();
+  }, 6000);
+}
+
+// async function start() {
+//   await startDownload();
+//   await startSpriteGeneration();
+//   await startUploadingSprite();
+//   console.log(`ALL Processes Completed`);
+// }
+
+prompt.start();
+
+prompt.get(['Coudinary_Folder_Name', 'Output_Sprite_File_Name'], (err, result) => {
+  folderName = result.Coudinary_Folder_Name || 'hevo-blog-dev';
+  outputFileName = result.Output_Sprite_File_Name || "output_sprite";
+
+  start();
+});
+
+// app.listen(process.env.PORT, () => console.log(`Server is running at PORT ${process.env.PORT}`));
+app.listen(process.env.PORT);
